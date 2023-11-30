9ee2d8b9e3ec6c82fe278b99423b9fdc70ce7b9c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-11-30 11:08:06 +0530

    first

---
+++ day4/webserver.js/webserver.js
@@ -0,0 +1,11 @@
+let http = require('http');
+let url = require('url');
+let functions = require('./functions');
+let port = 9000;
+let server = http.createServer(function(request, response) {
+let args = url.parse(request.url, true).query;
+response.end(functions.sayHello(args.personName));
+});
+server.listen(port, function() {
+console.log('Server listening on port ' + port);
+});
