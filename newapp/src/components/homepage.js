8f53bc9133a235376f5e83d3e7f3b597cf823e14
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-13 22:16:16 +0530

    added homepage

--- newapp/src/components/homepage.js
+++ newapp/src/components/homepage.js
@@ -1,9 +1,18 @@
 
 import {  Container, Box, Typography, Grid, Table, TableCell, TableHead,TableRow,TableBody } from "@mui/material";
 import { useNavigate } from 'react-router-dom';
+import MyComponent from "./navbar";
 
-const Home = ({props}) => {
+const Home = () => {
+    const navigate = useNavigate();
 
-    
+    return(
+        <Container>
+            <nabar>
+                <MyComponent/>
+            </nabar>
+        </Container>
+    )
+
 }
 export default Home;
