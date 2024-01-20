68467c85f08e47bb36db67a17d41cd25a7735cdc
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-20 23:59:35 +0530

    f

--- node/Server.js
+++ node/Server.js
@@ -1,21 +1,46 @@
+
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();
+
 
 const app = express();
 const port = 3000; // Change this to the desired port number
 
 app.use(express.json());
 
+const openaiKey = process.env.OPENAI_KEY;
 
-// Define a route to handle file uploads
-app.post('/upload', async (req, res) => {
-  res.json({ message: 'Hellow World'});
+app.post('/summarize', async (req, res) => {
+  try {
+    const { transcription } = req.body;
+
+    const headers = {
+      'Authorization': `Bearer ${openaiKey}`,
+      'Content-Type': 'application/json',
+    };
+
+    const data = {
+      prompt: `Chatgpt, I need you to summarize this text: ${transcription}`,
+      max_tokens: 400,
+      model: "gpt-3.5-turbo-instruct",
+    };
+
+    const url = 'https://api.openai.com/v1/completions';
+
+    const response = await axios.post(url, data, { headers });
+    const summary = response.data.choices[0].text;
+
+    res.json({ summary });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to summarize' });
+  }
 });
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
+   
+   app.listen(port, () => {
+     console.log(`Server is running on port ${port}`);
+   });
 
 
 ///run this before 
