5e8be8f752153d25d9328441a34c179f8c4d321f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-20 21:17:02 +0530

    chgned

--- node/Server.js
+++ node/Server.js
@@ -1,58 +1,20 @@
-require('dotenv').config();
-
-
 const express = require('express');
-
+const axios = require('axios');
 
 const app = express();
+const port = 3000; // Change this to the desired port number
+
 app.use(express.json());
-const { Configuration, OpenAIApi } = require("openai"); 
-const readlineSync = require("readline-sync"); 
-require("dotenv").config(); 
-
-let APIcall = async () => { 
-const newConfig = new Configuration({ 
-	apiKey: process.env.OPENAI_SECRET_KEY 
-}); 
-const openai = new OpenAIApi(newConfig); 
-	
-const chatHistory = []; 
-
-do { 
-	const user_input = readlineSync.question("Enter your input: "); 
-	const messageList = chatHistory.map(([input_text, completion_text]) => ({ 
-	role: "user" === input_text ? "ChatGPT" : "user", 
-	content: input_text 
-	})); 
-	messageList.push({ role: "user", content: user_input }); 
-
-	try { 
-	const GPTOutput = await openai.createChatCompletion({ 
-		model: "gpt-3.5-turbo", 
-		messages: messageList, 
-	}); 
-
-	const output_text = GPTOutput.data.choices[0].message.content; 
-	console.log(output_text); 
-
-	chatHistory.push([user_input, output_text]); 
-	} catch (err) { 
-	if (err.response) { 
-		console.log(err.response.status); 
-		console.log(err.response.data); 
-	} else { 
-		console.log(err.message); 
-	} 
-	} 
-} while (readlineSync.question("\nYou Want more Results? (Y/N)").toUpperCase() === "Y"); 
-}; 
-APIcall();
 
 
+// Define a route to handle file uploads
+app.post('/upload', async (req, res) => {
+  res.json({ message: 'Hellow World'});
+});
 
-
-const port = 3001;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 
 
