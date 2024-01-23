fec4d87d938a02e12b9100d067bbfc7e46d087db
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-23 14:48:40 +0530

    removed

--- node/Server.js
+++ node/Server.js
@@ -3,16 +3,6 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-import { ChatGPTAPI } from 'chatgpt'
 
-async function example() {
-  const api = new ChatGPTAPI({
-    apiKey: process.env.OPENAI_API_KEY
-  })
-
-  const res = await api.sendMessage('Hello World!')
-  console.log(res.text)
-}
-example();
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
