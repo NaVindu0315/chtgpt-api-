b89ec7484d3d5bcb60ef65013768802994526cbb
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-23 15:42:56 +0530

    finished functon

--- node/Server.js
+++ node/Server.js
@@ -10,8 +10,20 @@ const openai = new OpenAI(
     })
 );
 
-app.get('/getresponse',(req,res)=>{
+app.get('/getresponse',async(req,res)=>{
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
+        message: [{
+            
+            "role":"user",
+            "content":"esasay on global warming",
+            max_tokens: 100
+    
+                 }]
     })
+    console.log(response)
 })
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
