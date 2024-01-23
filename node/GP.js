11fb7d8bddd7f9d7048f8d4cd976c68cd02707f4
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-01-23 17:07:10 +0530

    now working prompt message need to be customized

--- node/GP.js
+++ node/GP.js
@@ -12,18 +12,19 @@ const openai = new OpenAI(
 
 app.get('/getresponse', async (req, res) => {
     try {
-        const userPrompt = req.body.userPrompt;
+        //const userPrompt = req.body.userPrompt;
         const studentname = req.body.studentname;
         const studentnumber = req.body.studentnumber;
         const studentdegree = req.body.studentdegree;
         const studentgpa = req.body.studentgpa;
         const studentsports = req.body.studentsports;
-        console.log(userPrompt);
+        const studetfaculty = req.body.studetfaculty;
+        console.log(studentname,studentnumber,studentdegree,studentgpa,studentsports);
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo',
             messages: [{
                 "role": "user",
-                "content": " i want write a essay about "+userPrompt
+                "content": "i want to write a letter about the a student nameed"+studentname+" with the student number "+studentnumber+" who studied "+studentdegree+" with a gpa of "+studentgpa+" and did  "+studentsports+" in our university  as a dean of the faculty as a recomdation for a masters degree and add recipent as Dear Sir/Madam sender is Dean Faculty of"+studetfaculty+"please dont add recipeints senders desgisnation would be engough please dont use [] in the spaces"
                
             }]
         });
