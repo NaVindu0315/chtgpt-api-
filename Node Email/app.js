7cb08499b99cd7a12c4699eab0e4dc4929d2185a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-27 00:42:13 +0530

    node email sending working with user prompt

--- Node Email/app.js
+++ Node Email/app.js
@@ -1,4 +1,4 @@
-const nodemailer = require ('nodemailer');
+/*const nodemailer = require ('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth:{
@@ -16,3 +16,43 @@ const mailOptions = {
 }
 
 transporter.sendMail(mailOptions);
+*/
+
+const nodemailer = require('nodemailer');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter recipient email: ', (to) => {
+  rl.question('Enter subject: ', (subject) => {
+    rl.question('Enter text: ', (text) => {
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'nmails6969@gmail.com',
+          pass: 'thusncqpaawegxai'
+        }
+      });
+
+      const mailOptions = {
+        from: 'nmails6969@gmail.com',
+        to: to,
+        subject: subject,
+        text: text,
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+      rl.close();
+    });
+  });
+});
